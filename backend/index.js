5b9538e103f1ac947982928151661e3fc649ec91
huy.tranminh <tmhuyy.itiu@gameloft.com>
2024-05-18 14:36:07 +0700

    - test

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,8 @@ require('dotenv').config()
 
 const app = require('./app')
 
-const PORT = process.env.PORT
+// const PORT = process.env.PORT
 
-app.listen(PORT, () => {
-    console.log(`APP IS RUNNING AT http://localhost:${PORT}`)
+app.listen(3000, () => {
+    console.log(`APP IS RUNNING AT http://localhost:3000`)
 })
